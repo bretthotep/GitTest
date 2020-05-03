39b562e200c9bceba80c7a72e5384c626143f8e2
Brett Irving <brett.irving@outlook.com>
2020-05-03 11:45:03 +1000

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Brett Irving on 3/5/20.
+//  Copyright © 2020 Brett Irving. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
